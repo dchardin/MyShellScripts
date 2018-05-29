e8f1f8b45c73e395ba0ea824891092bfeb6e12bf
dchardin <dch84121@gmail.com>
2018-05-29 13:58:19 -0400

    corrected issues with slowecho function. Works now.

--- slowEchoFunk.sh
+++ slowEchoFunk.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-
 slowecho ()
 {
-slowout="$1"
-expect -c 'set send_human {.1 .3 1 .05 2}';
-spawn echo "$env(slowout)";
+slowout="$1";
+export slowout;
+expect -c 'set send_human {.1 .3 1 .05 2};
+spawn -noecho echo $::env(slowout);
 interact -indices -o -re ".+" { send_user -h -- "$interact_out(0,string)" }'
 }
 
-slowecho "This is a test"
+slowecho "Disturbance indicates a synchronic distortion in the areas emanating triolic waves. The cerebellum, the cerebral cortex, the brain stem,  the entire nervous system has been depleted of electrochemical energy. Any device like that would produce high levels of triolic waves. These walls have undergone some kind of selective molecular polarization. I haven't determined if our phaser energy can generate a stable field. We could alter the photons with phase discriminators.
+
+Unidentified vessel travelling at sub warp speed, bearing 235.7. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam.
+
+Run a manual sweep of anomalous airborne or electromagnetic readings. Radiation levels in our atmosphere have increased by 3,000 percent. Electromagnetic and subspace wave fronts approaching synchronization. What is the strength of the ship's deflector shields at maximum output? The wormhole's size and short period would make this a local phenomenon. Do you have sufficient data to compile a holographic simulation?"
+
+
+#handy_settings
+
+#{.01 .02 5 .005 .005}
+#{.1 .3 1 .05 2}
